b833a9b3277e99c77ea4212895e0f9e1c83faf03
yewon <ipunwon_@naver.com>
2022-11-29 17:34:56 +0900

    36-40

---
+++ routes.js
@@ -0,0 +1,54 @@
+const fs = require("fs");
+const requrestHandler = (req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFile("message.txt", message, (err) => {
+        //경로 재지정
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
+  }
+
+  // console.log(req.url, req.method, req.headers);
+  //process.exit();
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
+  res.write("</html>");
+  res.end();
+};
+
+// module.exports = requrestHandler;
+
+// module.exports = {
+//   handler: requrestHandler,
+//   someText: "Some hard coded text",
+// };
+
+// module.exports.handler = requrestHandler;
+// module.exports.someText = "Some text";
+
+exports.handler = requrestHandler;
+exports.someText = "Some hard coded text";
