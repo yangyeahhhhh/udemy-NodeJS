fbd8788a1164fa7b2fc3708547c52a57a1f7772e
yangyeahhhhh <112038515+yangyeahhhhh@users.noreply.github.com>
2023-01-10 16:08:52 +0900

    68 - 77

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,18 @@
+const path = require('path');
+
 const express = require('express');
+
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
+// /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
-router.post('/product', (req, res, next) => {
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
   console.log(req.body);
   res.redirect('/');
 });
