6782571e46cbbe20a746f5cdf00792f47cbca687
yangyeahhhhh <112038515+yangyeahhhhh@users.noreply.github.com>
2023-02-03 23:26:15 +0900

    78 - 79

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin.js');
+const adminData = require('./routes/admin.js');
 const shopRoutes = require('./routes/shop.js');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
